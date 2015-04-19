aae25448ad5d996dc565d213a15dc239f7fff0af
Peter Huang <peter.huang@twseeds.com>
2015-04-19 19:58:55 +0800

    Memento v4

--- Memento/Ledger.swift
+++ Memento/Ledger.swift
@@ -11,89 +11,80 @@ class LedgerEntry {
 }
 
 class LedgerMemento : Memento {
+    let data:NSData;
     
-    let jsonData:String?;
-    
-    init(ledger:Ledger) {
-        self.jsonData = stringify(ledger);
-    }
-    
-    init(json:String?) {
-        self.jsonData = json;
-    }
-    
-    private func stringify(ledger:Ledger) -> String? {
-        var dict = NSMutableDictionary();
-        dict["total"] = ledger.total;
-        dict["nextId"] = ledger.nextId;
-        dict["entries"] = ledger.entries.values.array;
-        var entryArray = [NSDictionary]();
-        
-        for entry in ledger.entries.values {
-            var entryDict = NSMutableDictionary();
-            entryArray.append(entryDict);
-            entryDict["id"] = entry.id;
-            entryDict["counterParty"] = entry.counterParty;
-            entryDict["amount"] = entry.amount;
-        }
-        
-        dict["entries"] = entryArray;
-        if let jsonData = NSJSONSerialization.dataWithJSONObject(dict,
-            options: nil, error: nil) {
-            return NSString(data: jsonData, encoding: NSUTF8StringEncoding);
-        }
-        return nil;
-    }
-    
-    func apply (ledger:Ledger) {
-            
-        if let data = jsonData?.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
-            if let dict = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary {
-        
-                ledger.total = dict["total"] as Float;
-                ledger.nextId = dict["nextId"] as Int;
-                ledger.entries.removeAll(keepCapacity: true);
-                if let entryDicts = dict["entries"] as? [NSDictionary] {
-                    for dict in entryDicts {
-                        let id = dict["id"] as Int;
-                        let counterParty = dict["counterParty"] as String;
-                        let amount = dict["amount"] as Float;
-                        ledger.entries[id] = LedgerEntry(id: id,counterParty: counterParty, amount: amount);
-                    }
-                }
-            }
-        }
-    }
+    init(data:NSData) { self.data = data;}
 }
 
-class Ledger : Originator {
-            
-            private var entries = [Int:LedgerEntry]();
-            private var nextId = 1;
-            var total:Float = 0;
-            func addEntry(counterParty:String, amount:Float) {
-                let entry = LedgerEntry(id: nextId++, counterParty: counterParty, amount: amount);
-                entries[entry.id] = entry;
-                    total += amount;
-            }
-            
-            func createMemento() -> Memento {
-                return LedgerMemento(ledger: self);
-            }
-            
-            func applyMemento(memento: Memento) {
-                if let m = memento as? LedgerMemento {
-                    m.apply(self);
+class Ledger : NSObject, Originator, NSCoding {
+    private var entries = [Int:LedgerEntry]();
+    private var nextId = 1;
+    var total:Float = 0;
+    
+    override init() {
+        // do nothing - required to allow instances
+        // to be created without a coder
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        self.total = aDecoder.decodeFloatForKey("total");
+        self.nextId = aDecoder.decodeIntegerForKey("nextId");
+        self.entries.removeAll(keepCapacity: true);
+        if let entryArray = aDecoder.decodeDataObject()
+            as AnyObject? as? [NSDictionary] {
+                for entryDict in entryArray {
+                    let id = entryDict["id"] as Int;
+                    let counterParty = entryDict["counterParty"] as String;
+                    let amount = entryDict["amount"] as Float;
+                    self.entries[id] = LedgerEntry(id: id, counterParty: counterParty,
+                        amount: amount);
                 }
+        }
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeFloat(total, forKey: "total");
+        aCoder.encodeInteger(nextId, forKey: "nextId");
+        var entriesArray = [NSMutableDictionary]();
+        for entry in self.entries.values {
+            var dict = NSMutableDictionary();
+            dict["id"] = entry.id;
+            dict["counterParty"] = entry.counterParty;
+            dict["amount"] = entry.amount;
+            entriesArray.append(dict);
+        }
+        aCoder.encodeObject(entriesArray);
+    }
+    
+    func createMemento() -> Memento {
+        return LedgerMemento(data: NSKeyedArchiver.archivedDataWithRootObject(self));
+    }
+    
+    func applyMemento(memento: Memento) {
+        if let lmemento = memento as? LedgerMemento {
+            if let obj = NSKeyedUnarchiver.unarchiveObjectWithData(lmemento.data)
+                as? Ledger {
+                    self.total = obj.total;
+                    self.nextId = obj.nextId;
+                    self.entries = obj.entries;
             }
-            
-            func printEntries() {
-                for id in entries.keys.array.sorted(<) {
-                    if let entry = entries[id] {
-                        println("#\(id): \(entry.counterParty) $\(entry.amount)");
-                    }
-                }
-                println("Total: $\(total)");
-                println("----");
+        }
+    }
+    
+    func addEntry(counterParty:String, amount:Float) {
+        let entry = LedgerEntry(id: nextId++, counterParty: counterParty,
+            amount: amount);
+        entries[entry.id] = entry;
+        total += amount;
+    }
+    
+    func printEntries() {
+        for id in entries.keys.array.sorted(<) {
+            if let entry = entries[id] {
+                println("#\(id): \(entry.counterParty) $\(entry.amount)");
             }
+        }
+        println("Total: $\(total)");
+        println("----");
+    }
 }
--- Memento/main.swift
+++ Memento/main.swift
@@ -1,9 +1,10 @@
 let ledger = Ledger();
+
 ledger.addEntry("Bob", amount: 100.43);
 ledger.addEntry("Joe", amount: 200.20);
-let memento = ledger.createMemento() as LedgerMemento;
-let newMemento = LedgerMemento(json: memento.jsonData);
-ledger.addEntry("Joe2", amount: 300.20);
-ledger.printEntries();
-ledger.applyMemento(newMemento);
+
+let memento = ledger.createMemento();
+
+ledger.applyMemento(memento);
+
 ledger.printEntries();
