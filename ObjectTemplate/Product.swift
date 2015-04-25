7f1a637e48cd875565d93d780c61f6ef7e728161
Peter Huang <peter.huang@twseeds.com>
2015-04-25 15:25:05 +0800

    object template v2

--- ObjectTemplate/Product.swift
+++ ObjectTemplate/Product.swift
@@ -1,31 +1,10 @@
 class Product {
-    
     var name:String;
     var price:Double;
-    private var stockBackingValue:Int = 0;
-    
-    var stock:Int {
-        get {
-            return stockBackingValue;
-        }
-        set {
-            stockBackingValue = max(0, newValue);
-        }
-    }
-    
+    var stock:Int;
     init(name:String, price:Double, stock:Int) {
-        self.name = name;
-        self.price = price;
-        self.stock = stock;
-    }
-    
-    func calculateTax(rate: Double) -> Double {
-        return min(10, self.price * rate);
-    }
-    
-    var stockValue: Double {
-        get {
-            return self.price * Double(self.stock);
-        }
+    self.name = name;
+    self.price = price;
+    self.stock = stock;
     }
 }
--- ObjectTemplate/main.swift
+++ ObjectTemplate/main.swift
@@ -1,12 +1,17 @@
-var products = [("Kayak", 275.0, 10), ("Lifejacket", 48.95, 14), ("Soccer Ball", 19.5, 32)];
+var products = [
+    Product(name: "Kayak", price: 275, stock: 10),
+    Product(name: "Lifejacket", price: 48.95, stock: 14),
+    Product(name: "Soccer Ball", price: 19.5, stock: 32)];
 
-func calculateTax(product:(String, Double, Int)) -> Double {
-    return product.1 * 0.2;
+func calculateTax(product:Product) -> Double {
+    return product.price * 0.2;
+}
+func calculateStockValue(productsArray:[Product]) -> Double {
+    return productsArray.reduce(0, {(total, product) -> Double in
+    return total + (product.price * Double(product.stock))
+    });
 }
 
-func calculateStockValue(tuples:[(String, Double, Int)]) -> Double {
-    return tuples.reduce(0, {(total, product) -> Double in
-    return total + (product.1 * Double(product.2))
-    }); }
+
 println("Sales tax for Kayak: $\(calculateTax(products[0]))");
 println("Total value of stock: $\(calculateStockValue(products))");
